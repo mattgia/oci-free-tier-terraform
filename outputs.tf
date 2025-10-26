75dff035c6d94220edbfdc1456f8442fe8310467
Matthew Giaconelli <mattgia3@gmail.com>
2025-10-26 15:28:55 -0700

    cleanup

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ssh_connection_command" {
-  description = "Command to create SSH session through bastion"
-  value       = "oci bastion session create --bastion-id ${oci_bastion_bastion.bastion.id} --target-private-ip ${oci_core_instance.free_instance.private_ip} --ssh-public-key-file <path_to_your_public_key>"
-}
