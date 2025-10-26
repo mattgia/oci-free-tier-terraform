60e3ac6edb7d199c55a0c39c0ca1fb76167c0140
Matthew Giaconelli <mattgia3@gmail.com>
2025-10-26 12:28:14 -0700

    update route table

--- network.tf
+++ network.tf
@@ -13,19 +13,24 @@ resource "oci_core_subnet" "subnet" {
   cidr_block       = "10.0.1.0/24"
   display_name     = "private-subnet"
   dns_label        = "privatesubnet"
-  route_table_id    = oci_core_route_table.route_table.id
+  route_table_id    = oci_core_route_table.private_route_table.id
 }
 
-# Create a route table
-resource "oci_core_route_table" "route_table" {
+# Create a route table for the private subnet
+resource "oci_core_route_table" "private_route_table" {
   compartment_id = var.tenancy_ocid
   vcn_id         = oci_core_vcn.vcn.id
-  display_name   = "route-table"
+  display_name   = "private-route-table"
 
-  # Add route rules if needed
-  # For example, for internet access:
-  # route_rules {
-  #   destination       = "0.0.0.0/0"
-  #   network_entity_id = oci_core_internet_gateway.ig.id
-  # }
+  route_rules {
+    destination       = "0.0.0.0/0"
+    network_entity_id = oci_core_nat_gateway.nat_gateway.id
+  }
+}
+
+# Create a NAT Gateway
+resource "oci_core_nat_gateway" "nat_gateway" {
+  compartment_id = var.tenancy_ocid
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "nat-gateway"
 }
--- provider.tf
+++ provider.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 variable "tenancy_ocid" {}
-
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
