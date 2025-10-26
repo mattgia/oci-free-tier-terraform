8466d9baf2e0dc983e7bfe7dc04322a8c988d7db
Matthew Giaconelli <mattgia3@gmail.com>
2025-10-26 12:53:15 -0700

    state storage config

--- provider.tf
+++ provider.tf
@@ -1,4 +1,16 @@
 terraform {
+  backend "s3" {
+    bucket   = "terraform-state-storage"
+    key      = "terraform.tfstate"
+    region   = "ca-toronto-1"
+    endpoints = {
+      s3 = "https://namespace.compat.objectstorage.ca-toronto-1.oraclecloud.com"
+    }
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
   required_providers {
     oci = {
       source  = "oracle/oci"
--- variables.tf
+++ variables.tf
@@ -1,10 +1,28 @@
-# We'll use the tenancy OCID as the root compartment ID, which is provided via the OCI_TENANCY_OCID environment variable
+# We'''ll use the tenancy OCID as the root compartment ID, which is provided via the OCI_TENANCY_OCID environment variable
 
-# Removed availability_domain variable as we're using data source
+# Removed availability_domain variable as we'''re using data source
 
-# Removed subnet_id variable as we're creating the subnet in network.tf
+# Removed subnet_id variable as we'''re creating the subnet in network.tf
 
 variable "ssh_public_key" {
   description = "SSH public key for instance access"
   type        = string
+}
+
+variable "bucket" {
+  description = "The name of the OCI Object Storage bucket for the Terraform state."
+  type        = string
+  default     = "my-bucket"
+}
+
+variable "key" {
+  description = "The path and name of the Terraform state file in the OCI Object Storage bucket."
+  type        = string
+  default     = "terraform.tfstate"
+}
+
+variable "namespace" {
+  description = "The OCI Object Storage namespace."
+  type        = string
+  default     = "my-namespace"
 }
